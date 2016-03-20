62a5556dd070037d32f9601b10e78e3aa95f1286
ReedKendall <kendallrh@gmail.com>
2016-03-20 16:31:46 -0500

    Changes YO
    Started RayCast and eliminated physics.

--- State_Machine_and_Movement/ScoutStates.cs
+++ State_Machine_and_Movement/ScoutStates.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ScoutStates : MonoBehaviour {
 
 	public bool grounded;
+	private GameObject planet;
 
 	enum GameState {Idle, Duck, Jump, AirMove, FastFall, FastMoveFall, LongJump, Run, Roll, Crawl}
 
@@ -12,11 +13,13 @@ public class ScoutStates : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		SetCurrentState (GameState.Idle);
+		planet = GameObject.FindGameObjectWithTag ("Planet");
 	}
 
 	void SetCurrentState(GameState state) {
 		currentState = state;
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,6 +33,7 @@ public class ScoutStates : MonoBehaviour {
 			}
 			if (Input.GetKeyDown (KeyCode.A) || Input.GetKey (KeyCode.D)) {
 				SetCurrentState (GameState.Run);
+				Raycasting ();
 				break;
 			}
 			if (Input.GetKeyDown (KeyCode.S)) {
@@ -58,8 +62,9 @@ public class ScoutStates : MonoBehaviour {
 			}
 			break;
 		case GameState.Jump:
-			transform.Translate (Vector2.up * 100 * Time.deltaTime);
 			if (grounded == true) {
+				transform.Translate (Vector2.up * 1 * Time.deltaTime);
+
 				if (Input.anyKey == false) {
 					SetCurrentState (GameState.Idle);
 					break;
@@ -115,6 +120,8 @@ public class ScoutStates : MonoBehaviour {
 			}
 			break;
 		case GameState.Run:
+			Raycasting ();
+			Run ();
 			if (Input.anyKey == false) {
 				SetCurrentState (GameState.Idle);
 				break;
@@ -139,6 +146,46 @@ public class ScoutStates : MonoBehaviour {
 			
 			break;
 		}
+	}
+
+	void Rotate() {
+		//Vector2 pos = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+		//Vector2 ppos = new Vector2 (planet.transform.position.x, planet.transform.position.y);
+		//transform.rotation = Quaternion.LookRotation ((pos - ppos).normalized);
+	}
+
+	void Run() {
+		if (Input.GetKey (KeyCode.A)) {
+			Vector2 dir = planet.transform.position - gameObject.transform.position;
+			float angle = (Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg) + 90;
+			transform.Translate (Vector2.left * 10 * Time.deltaTime);
+			transform.RotateAround (planet.transform.position, Vector3.forward, 100 * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector2.right * 10 * Time.deltaTime);
+		}
+	}
+
+	void Raycasting() {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			BoxCollider2D col = GetComponent<BoxCollider2D> ();
+			Vector2 tpos = new Vector2 (transform.position.x, transform.position.y + col.bounds.extents.y);
+			Vector2 bpos = new Vector2 (transform.position.x, transform.position.y - col.bounds.extents.y);
+			RaycastHit2D thit = Physics2D.Raycast (tpos, Vector2.left, 10, LayerMask.GetMask("Environments"));
+			RaycastHit2D bhit = Physics2D.Raycast (bpos, Vector2.left, 10, LayerMask.GetMask("Environments"));
+			Debug.DrawRay (tpos, Vector2.left * 10, Color.white, 1);
+			Debug.DrawRay (bpos, Vector2.left * 10, Color.white, 1);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			BoxCollider2D col = GetComponent<BoxCollider2D> ();
+			Vector2 tpos = new Vector2 (transform.position.x, transform.position.y + col.bounds.extents.y);
+			Vector2 bpos = new Vector2 (transform.position.x, transform.position.y - col.bounds.extents.y);
+			RaycastHit2D thit = Physics2D.Raycast (tpos, Vector2.left, 10, LayerMask.GetMask("Environments"));
+			RaycastHit2D bhit = Physics2D.Raycast (bpos, Vector2.left, 10, LayerMask.GetMask("Environments"));
+			Debug.DrawRay (tpos, Vector2.right * 10, Color.red, 1);
+			Debug.DrawRay (bpos, Vector2.right * 10, Color.red, 1);
+		}
+
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
